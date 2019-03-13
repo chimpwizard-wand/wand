ba21fb319ed497b9c06b9abe291e2e6a13f80d66
ndruwizz <ndru@chimpwizard.com>
2019-03-13 05:00:24 -0400

    last updates

---
+++ README.md
@@ -0,0 +1,20 @@
+# Wand
+
+```yaml
+by: иÐгü
+email: ndru@chimpwizard.com
+date: 03.07.2019
+version: draft
+```
+
+---
+
+This wand/cli is inspired and forked from meta cli.
+
+Eventually this code will be proposed to get merged inso meta core code.
+
+## Available spells
+
+- solution: This scafold an application based on a predefined stack.
+- github: Wrapper over github API.
+- vault: Secret management available for other spells that might need to access secrets/passwords variables.
--- index.js
+++ index.js
@@ -1,48 +1,62 @@
-const { gray, green } = require('chalk');
-const debug = require('debug')('meta');
-const fs = require('fs');
-const path = require('path');
+const { gray, green } = require("chalk");
+const debug = require("debug")("wand");
+const fs = require("fs");
+const path = require("path");
 
-const meta = require('./package.json');
-const findPlugins = require('./lib/findPlugins');
-const registerPlugin = require('./lib/registerPlugin');
+const wand = require("./package.json");
+const findSpells = require("./lib/findSpells");
+const registerSpell = require("./lib/registerSpell");
 
 // Plugins depended on by the `meta` package.
 const corePlugins = new Map();
-Object.keys(meta.dependencies).forEach(name => {
+Object.keys(wand.dependencies).forEach(name => {
   if (/^meta-/.test(name)) {
-    const packagePath = require.resolve(path.join(name, 'package.json'));
+    const packagePath = require.resolve(path.join(name, "package.json"));
+    corePlugins.set(name, path.dirname(packagePath));
+  }
+  if (/^@chimpwizard-wand/.test(name)) {
+    const packagePath = require.resolve(path.join(name, "package.json"));
     corePlugins.set(name, path.dirname(packagePath));
   }
 });
 
-exports.version = meta.version;
+//console.log("VERSION: " + wand.version);
+debug("CORE Spells are: " + JSON.stringify(corePlugins));
+
+exports.version = wand.version;
 
 exports.run = (cwd, argv) => {
-  const program = require('commander').version(meta.version);
+  const program = require("commander").version(wand.version);
 
   // Ensure `cwd` is actually the working directory.
   cwd = path.resolve(cwd);
   process.chdir(cwd);
 
   // Load user plugins.
-  const userPlugins = findPlugins(cwd);
+  const userPlugins = findSpells(cwd);
+  debug("INSTALLED Spells are: " + JSON.stringify(userPlugins));
+
   if (userPlugins.size) {
     debug(`\nLoading plugins:`);
-    userPlugins.forEach(pluginPath => registerPlugin(program, pluginPath));
+    userPlugins.forEach(pluginPath => registerSpell(program, pluginPath));
   }
 
   // Load core plugins after, so users can override them.
   debug(`\nLoading core plugins:`);
   corePlugins.forEach((pluginPath, name) => {
     if (userPlugins.has(name)) return debug(`  ${green('+')} ${name} ${gray('(skip)')}`); // prettier-ignore
-    registerPlugin(program, pluginPath);
+    registerSpell(program, pluginPath);
   });
 
-  if (fs.existsSync('.meta')) {
-    const gitPlugin = userPlugins.get('meta-git') || 'meta-git';
+  debug(`\nChecking .meta:`);
+  if (fs.existsSync(".meta")) {
+    const gitPlugin =
+      userPlugins.get("@chimpwizard-wand/spell-git") ||
+      "@chimpwizard-wand/spell-git";
+    debug(`\nLoading into memory @chimpwizard-wand/spell-git`);
     require(gitPlugin).update({ dryRun: true });
   }
 
+  debug("EXECUTING: " + JSON.stringify(argv));
   program.parse(argv);
 };
